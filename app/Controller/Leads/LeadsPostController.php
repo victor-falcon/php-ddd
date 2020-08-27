9ce655add984243ff5eb103e4285b087a15cdfe5
Víctor Falcón <victoor89@gmail.com>
2020-08-27 15:05:27 +0200

    Return 400 bad request if lead email it's duplicated

--- app/Controller/Leads/LeadsPostController.php
+++ app/Controller/Leads/LeadsPostController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Leads;
 
 use Cal\Leads\Command\CreateLeadJob;
 use Cal\Leads\Command\CreateLeadJobHandler;
+use Cal\Leads\Domain\Exception\DuplicatedLeadException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,8 +23,11 @@ class LeadsPostController
         $name = $request->get('name');
         $email = $request->get('email');
 
-        ($this->handler)(new CreateLeadJob($name, $email));
-
-        return new Response(null, Response::HTTP_CREATED);
+        try {
+            ($this->handler)(new CreateLeadJob($name, $email));
+            return new Response(null, Response::HTTP_CREATED);
+        } catch (DuplicatedLeadException $e) {
+            return new Response(null, Response::HTTP_BAD_REQUEST);
+        }
     }
 }
